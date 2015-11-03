d8f2b7c3ae25b177e793599341baffcc1c88c598
Kevin Jung <kjung@expedia.com>
2015-11-02 18:03:20 -0800

    Upgrading dbcp 2.1.1 and setting deault query timeout

--- data-access/src/main/java/com/alicesfavs/dataaccess/impl/DaoSupport.java
+++ data-access/src/main/java/com/alicesfavs/dataaccess/impl/DaoSupport.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
@@ -26,6 +27,8 @@ public class DaoSupport<T> extends JdbcDaoSupport
     private final String COLUMN_CREATED_DATE = "created_date";
     private final String COLUMN_UPDATED_DATE = "updated_date";
 
+    private static final int DEFAULT_QUERY_TIMEOUT = 5;
+
     @Autowired
     public DaoSupport(DataSource dataSource)
     {
@@ -39,7 +42,7 @@ public class DaoSupport<T> extends JdbcDaoSupport
         final PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql, types);
         factory.setReturnGeneratedKeys(true);
         factory.setGeneratedKeysColumnNames(COLUMN_ID, COLUMN_CREATED_DATE, COLUMN_UPDATED_DATE);
-        getJdbcTemplate().update(factory.newPreparedStatementCreator(params), keyHolder);
+        getJdbcTemplate(DEFAULT_QUERY_TIMEOUT).update(factory.newPreparedStatementCreator(params), keyHolder);
 
         Map<String, Object> result = keyHolder.getKeys();
         final ModelBase modelBase = new ModelBase((Long) result.get(COLUMN_ID),
@@ -55,7 +58,7 @@ public class DaoSupport<T> extends JdbcDaoSupport
         final PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql, types);
         factory.setReturnGeneratedKeys(true);
         factory.setGeneratedKeysColumnNames(COLUMN_UPDATED_DATE);
-        getJdbcTemplate().update(factory.newPreparedStatementCreator(params), keyHolder);
+        getJdbcTemplate(DEFAULT_QUERY_TIMEOUT).update(factory.newPreparedStatementCreator(params), keyHolder);
 
         Map<String, Object> result = keyHolder.getKeys();
 
@@ -65,25 +68,38 @@ public class DaoSupport<T> extends JdbcDaoSupport
     final int delete(String sql, int[] types, Object[] params)
     {
         final PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql, types);
-        return getJdbcTemplate().update(factory.newPreparedStatementCreator(params));
+        return getJdbcTemplate(DEFAULT_QUERY_TIMEOUT).update(factory.newPreparedStatementCreator(params));
     }
 
     final int updateMultiple(String sql, int[] types, Object[] params)
     {
         final PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql, types);
-        return getJdbcTemplate().update(factory.newPreparedStatementCreator(params));
+        return getJdbcTemplate(DEFAULT_QUERY_TIMEOUT).update(factory.newPreparedStatementCreator(params));
     }
 
     final T selectObject(String sql, int[] types, Object[] params, RowMapper<T> rowMapper)
     {
-        return getJdbcTemplate().queryForObject(sql, params, types, rowMapper);
+        return getJdbcTemplate(DEFAULT_QUERY_TIMEOUT).queryForObject(sql, params, types, rowMapper);
     }
 
     final List<T> selectObjectList(String sql, int[] types, Object[] params, RowMapper<T> rowMapper)
     {
+        return selectObjectList(sql, types, params, rowMapper, DEFAULT_QUERY_TIMEOUT);
+    }
+
+    final List<T> selectObjectList(String sql, int[] types, Object[] params, RowMapper<T> rowMapper, int queryTimeoutSeconds)
+    {
         final PreparedStatementCreatorFactory factory = new PreparedStatementCreatorFactory(sql, types);
 
-        return getJdbcTemplate().query(factory.newPreparedStatementCreator(params), rowMapper);
+        return getJdbcTemplate(queryTimeoutSeconds).query(factory.newPreparedStatementCreator(params), rowMapper);
+    }
+
+    private JdbcTemplate getJdbcTemplate(int timeoutSeconds)
+    {
+        final JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        jdbcTemplate.setQueryTimeout(timeoutSeconds);
+
+        return jdbcTemplate;
     }
 
 }
